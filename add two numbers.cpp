c69151460a26ad26e87bd611439a374af3a45d86
MOHAMMED YAMEEN S M <109512433+yamyy123@users.noreply.github.com>
2023-03-26 11:24:35 +0530

    Create add two numbers.cpp

---
+++ add two numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int c;
+    cin>>c;
+    for(int i=0;i<c;i++)
+    {
+       long long int a,b;
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+    return 0;
+}
