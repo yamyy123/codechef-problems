541d2a3a2ee41d225ba41b61fc68abdea1ff92b6
MOHAMMED YAMEEN S M <109512433+yamyy123@users.noreply.github.com>
2023-03-28 14:20:31 +0530

    Create squats.cpp

---
+++ squats.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long int a;
+	cin>>a;
+	for(int i=0;i<a;i++)
+	{
+	    long long int b;
+	    cin>>b;
+	    cout<<b*15<<endl;
+	}
+	return 0;
+}
