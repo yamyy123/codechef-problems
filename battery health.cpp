e81cc4a69bdde3f576cb89a9022c3b1553200d22
MOHAMMED YAMEEN S M <109512433+yamyy123@users.noreply.github.com>
2023-03-28 14:02:28 +0530

    Create battery health.cpp

---
+++ battery health.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cin>>a;
+	for(int i=0;i<a;i++)
+	{
+	    int b;
+	    cin>>b;
+	    if(b>=80)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
